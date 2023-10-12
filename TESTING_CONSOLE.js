dedf9cd78f867c6c615b40137cde3f05c256936e
TheHamBurgerler <134538594+THeHamBurgueler@users.noreply.github.com>
2023-10-12 07:33:02 -0400

    Update TESTING_CONSOLE.js

--- TESTING_CONSOLE.js
+++ TESTING_CONSOLE.js
@@ -1,40 +1,43 @@
 var htmlContent = `
-<html lang="en-us">
-  <head>
-    <link rel="stylesheet" href="https://ujosd1.csb.app/styles.css" />
-    <title>project no more</title>
-  </head>
-  <body>
-    <div class="overlay"></div>
-    <div class="stars" aria-hidden="true"></div>
-    <div class="starts2" aria-hidden="true"></div>
-    <div class="stars3" aria-hidden="true"></div>
-    <main class="main">
-      <section class="contact">
-        <h1 class="title">
-          project no more
-        </h1>
-        <p>
-          project no more is a website to find all of the best hacks for the
-          most popular school websites.<br />
-          <a href="/index.html">home</a> -
 
-          <a href="https://discord.gg/wcaRv5SaGN">discord</a>
-          <div class="hack" />
-        </p>
-        <hr />
-       
-        <br />
-        <hr />
-        <h2>new directories</h2>
-        <a href="https://lc64tw.csb.app/HowToUse.html">how to use the hacks</a><br>
-        <a href="hacks/blooket.html">blooket (W.I.P)</a><br>
-        <a href="https://lc64tw.csb.app/DeltamathHacks.html">delta math</a><br>
-        <a href="https://lc64tw.csb.app/EdpuzzleHacks.html">edpuzzle</a><br>
-      </section>
-    </main>
-  </body>
-</html>
+<html lang="en-us">
+    <head>
+      <link rel="stylesheet" href="https://ujosd1.csb.app/styles.css" />
+      <title>project no more</title>
+    </head>
+    <body>
+      <div class="overlay"></div>
+      <div class="stars" aria-hidden="true"></div>
+      <div class="starts2" aria-hidden="true"></div>
+      <div class="stars3" aria-hidden="true"></div>
+      <main class="main">
+        <section class="contact">
+          <h1 class="title">
+            project no more
+          </h1>
+          <p>
+            project no more is a website to find all of the best hacks for the
+            most popular school websites.<br />
+            <a href="/index.html">home</a> -
+  
+            <a href="https://discord.gg/wcaRv5SaGN">discord</a>
+            <div class="hack" />
+          </p>
+          <hr />
+         
+          <br />
+          <hr />
+          <h2>new directories</h2>
+          <a href="https://lc64tw.csb.app/HowToUse.html">how to use the hacks</a><br>
+          <a href="hacks/blooket.html">blooket (W.I.P)</a><br>
+          <a href="https://lc64tw.csb.app/DeltamathHacks.html">delta math</a><br>
+          <a href="https://lc64tw.csb.app/EdpuzzleHacks.html">edpuzzle</a><br>
+        </section>
+      </main>
+    </body>
+  </html>
+  
+   
 
 `;
 
